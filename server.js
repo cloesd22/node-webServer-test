b70c277774c03e36eb284c5ed689ed9b1110b554
cloesd22 <cloesd22@gmail.com>
2017-09-17 14:39:39 +1000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+hbs.registerHelper('yearhelper',()=>{
+	return new Date().getFullYear();
+})
+
+hbs.registerHelper('yeller',(text)=>{
+	return text.toUpperCase();
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+	var now = new Date().toString();
+
+	var displayString = `${now} -- ${req.url} -- ${req.method}`;
+
+	fs.appendFile('serverlog.txt',displayString + "\n",(error)=>{
+		if(error){
+			console.log("errormen: "+error);
+		}
+	});
+	console.log(now + " -- " + req.url + " -- "+ req.method);
+	next();
+});
+
+app.use((req,res,next)=>{
+	res.render('maintainence.hbs');
+})
+
+app.get('/about',(req,res)=>{
+
+	res.render('about.hbs',{
+		name:'Yusb00ss'
+	});
+});
+
+
+app.get('/bad',(req,res)=>{
+	res.send({
+		code: 3582,
+		message: "Ya dun goofed"
+	})
+});
+
+app.get('/',(req,res)=>{
+
+	date = new Date().getFullYear();
+
+	res.render('home.hbs',{
+		name:'Home',
+		currentYear:date,
+		welcomeMsg: 'Welcome home you '+date+' boiii!'
+	});
+})
+
+app.listen(3000,()=>{
+	console.log("we in bizniz");
+});
